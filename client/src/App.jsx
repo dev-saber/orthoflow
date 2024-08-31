dc07b4df2bcb932d8a3a583dade4c34dfbe90178
Sifeddine Eddarai <sifeddine.edr@gmail.com>
2024-08-31 23:55:29 +0100

    react router set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,61 @@
-export default function App() {
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Login from "./pages/Login";
+import Unauthorized from "./pages/Unauthorized";
+import NotFound from "./pages/NotFound";
+
+const AppLayout = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="flex flex-col">
+      <div className="mt-20 px-10 py-5">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default function App() {
+  const routing = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/Unauthorized",
+      element: <Unauthorized />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+    // {
+    //   path: "/",
+    //   element: <AuthMiddleware component={AppLayout} />,
+    //   children: [
+    //     {
+    //       path: "librarian",
+    //       children: [
+    //         {
+    //           path: "dashboard",
+    //           element: <Auth component={LibrarianAdminDashboard} />,
+    //         },
+    //         {
+    //           path: "books",
+    //           element: <Auth component={Books} />,
+    //         },
+    //         {
+    //           path: "borrows",
+    //           element: <Auth component={Borrow} />,
+    //         },
+    //         {
+    //           path: "profile",
+    //           element: <Auth component={Profile} />,
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
+  ]);
+
+  return <RouterProvider router={routing} />;
 }
