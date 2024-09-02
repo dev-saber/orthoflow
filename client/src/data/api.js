2f596c0da7ca7133dd9b69eac4254259e3ce8693
Sifeddine Eddarai <sifeddine.edr@gmail.com>
2024-09-02 19:42:45 +0100

    axios interceptors for authentication and error handling

--- client/src/data/api.js
+++ client/src/data/api.js
@@ -1,10 +1,42 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: "http://localhost:8000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response; // 200 status code responses are returned
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status == 401) {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+      } else {
+        console.error("An error occurred:", error.response.data);
+      }
+    } else {
+      console.error("An error occurred:", error.message);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
