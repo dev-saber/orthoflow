5353e05317523fc2a2c6e713bfd0f61b014a4729
Sifeddine Eddarai <sifeddine.edr@gmail.com>
2024-09-21 00:11:30 +0100

    stats component update

--- client/src/components/atoms/Stats.jsx
+++ client/src/components/atoms/Stats.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Stats({ beginText, data, endText }) {
+  const navigate = useNavigate();
+  const handleNavigation = () => {
+    navigate("/bills");
+    // filter only unpaid bills later
+  };
+
   return (
-    <div className="shadow-sm border border-solid rounded-lg border-gray-200 w-3/4 p-8 text-xl text-center">
+    <div
+      className="shadow-sm border border-solid rounded-lg border-gray-200 w-3/4 p-8 text-xl text-center cursor-pointer hover:bg-gray-50 hover:bg-opacity-50"
+      onClick={handleNavigation}
+    >
       {beginText}
       <span className="text-blue font-medium text-2xl px-2">{data}</span>
       {endText}
